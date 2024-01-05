6271cc7290f1122570bb8f2a72a367ca52d51198
Ksnapeek <81442945+KnapiCZ@users.noreply.github.com>
2024-01-05 10:02:43 +0100

    zmena importu obrazku + filtr
    filter jeste neni zapojen ale je v kodu

--- GUI/src/main/java/com/example/gui/HelloApplication.java
+++ GUI/src/main/java/com/example/gui/HelloApplication.java
@@ -3,11 +3,8 @@ package com.example.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -16,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Tvoje mama");
+        stage.setTitle("Image Filters");
         stage.setMinHeight(650.0);
         stage.setMinWidth(800.0);
         stage.setScene(scene);
--- GUI/src/main/java/com/example/gui/HelloController.java
+++ GUI/src/main/java/com/example/gui/HelloController.java
@@ -1,22 +1,27 @@
 package com.example.gui;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 import javafx.scene.media.*;
+import javafx.util.Duration;
+
 import java.io.File;
 
 public class HelloController {
     @FXML
     public Button fileButton;
     public TextArea appConsole;
-
+    @FXML
+    public ImageView importedImage;
     @FXML
     public Pane myImage;
 
@@ -42,24 +47,39 @@ public class HelloController {
         File selectedFile = fileChooser.showOpenDialog(window);
 
         if (selectedFile != null) {
-            String path = selectedFile.toURI().toString();
-            String style = "-fx-background-image: url(" + path + ")";
-            myImage.setStyle(style);
-            addTextToConsole("Image Loaded Succesfully");
-
+                String path = selectedFile.toURI().toString();
+                Image image = new Image(path);
+                importedImage.setImage(image);
+                addTextToConsole("Image Loaded Succesfully");
         }
     }
 
+    protected void filterGray() {
+        ColorAdjust colorAdjust = new ColorAdjust();
+        colorAdjust.setSaturation(44); // increase red by 50%
+        importedImage.setEffect(colorAdjust);
+        FadeTransition fade = new FadeTransition(Duration.seconds(2), importedImage);
+        fade.setFromValue(1.0); // fully opaque
+        fade.setToValue(0.0); // fully transparentfade.play();
+        ColorAdjust grayscale = new ColorAdjust();
+        grayscale.setSaturation(-1.0); // fully desaturatedImageView imageView = new ImageView(new Image("path/to/image.png"));
+        importedImage.setEffect(grayscale);
+    }
+
     protected  void playMeme(){
-        String musicFile = "GUI/src/main/resources/com/example/gui/josh.mp3";     // For example
+        String musicFile = "GUI/src/main/resources/com/example/gui/josh.mp3";     // For ksnapimu to funguje s GUI a lubosovi bez :)
 
         Media sound = new Media(new File(musicFile).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(sound);
-        mediaPlayer.setVolume(50);
         mediaPlayer.play();
     }
 
+
+
     @FXML
     public void initialize() {
-        playMeme();    }
+        playMeme();
+        importedImage.fitWidthProperty().bind(myImage.widthProperty());
+        importedImage.fitHeightProperty().bind(myImage.heightProperty());
+    }
 }
